629a11378ae089af78673affe8288fcf9c13ea3f
tarkxan <tarkxankut@gmail.com>
2021-02-19 23:02:59 -0500

    registered all routes in app.js file

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 // add express from node_modules
+const { response } = require('express')
 const express = require('express')
+
+// import authenrication
+const auth_routes = require('./routes/authentication')
+const analytics_routes = require('./routes/analytics')
+const category_routes = require('./routes/category')
+const order_routes = require('./routes/order')
+const position_routes = require('./routes/position')
 
 // add instance pf a server
 const app = express()
 
+// auth_routes has '/login', that's why full path will be '/api/auth/login
+app.use('/api/auth', auth_routes)
+app.use('/api/analytics', analytics_routes)
+app.use('/api/category', category_routes)
+app.use('/api/order', order_routes)
+app.use('/api/position', position_routes)
+
 let success_code = 200
 let root_dir = '/'
 let success_message = 'Working'
-
-// to check in browser that browser is working
-app.get(root_dir, (request, response) => {
-    response.status(success_code).json({
-        message: success_message
-    })
-})
 
 module.exports = app
