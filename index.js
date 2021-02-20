04eae9e132de3967e973af8b645844bc9fd85b8a
tarkxan <tarkxankut@gmail.com>
2021-02-19 21:29:11 -0500

    created app.js file to have server properties / index.js is only responsible for server run

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
+const app = require('./app')
+const port = process.env.PORT || 5000
 
-// add express from node_modules
-const express = require('express')
-
-// add instance pf a server
-const app = express()
-
-let success_code = 200
-let root_dir = '/'
-let success_message = 'Working'
-
-// to check in browser that browser is working
-app.get(root_dir, (request, response) => {
-    response.status(success_code).json({
-        message: success_message
-    })
-})
-
-// run server
-let port = 5000;
 app.listen(port, () => {
-    console.log('Server is running..')
+    console.log(`Server is running on port ${port}`)
 })
